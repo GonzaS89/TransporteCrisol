c49b1ca7a0dc549772a53191b59e45dbcbdbe5eb
GonzaS89 <gnzx89@gmail.com>
2022-09-09 14:04:37 -0300

    ajuste de tarifa

--- main.js
+++ main.js
@@ -134,79 +134,79 @@ function mostrarFlete(){
         }
         else{
             if(Math.ceil(valorIng) >=1 && valorIng <= 10){
-                resul=642;
+                resul=783,20;
             }
             if(Math.ceil(valorIng) >10 && valorIng <=20){
-                resul=756;
+                resul=922,30;
             }
             if(Math.ceil(valorIng) >20 && valorIng <=30){
-                resul=1022;
+                resul=1246,80;
             }
             if(Math.ceil(valorIng) >30 && valorIng <=40){
-                resul=1271;
+                resul=1550,60;
             }
             if(Math.ceil(valorIng) >40 && valorIng <=50){
-                resul=1501;
+                resul=1831.20;
             }
             if(Math.ceil(valorIng) >50 && valorIng <=60){
-                resul=1716;
+                resul=2093.50;
             }
             if(Math.ceil(valorIng) >60 && valorIng <=70){
-                resul=1952;
+                resul=2381.40;
             }
             if(Math.ceil(valorIng) >70 && valorIng <=80){
-                resul=2135;
+                resul=2604.70;
             }
             if(Math.ceil(valorIng) >80 && valorIng <=90){
-                resul=2266;
+                resul=2764.50;
             }
             if(Math.ceil(valorIng) >90 && valorIng <=100){
-                resul=2429;
+                resul=2963.30;
             }
             if(Math.ceil(valorIng) >100 && valorIng <=120){
-                resul=2757;
+                resul=3363.50;
             }
             if(Math.ceil(valorIng) >120 && valorIng <=140){
-                resul=3072;
+                resul=3747.80;
             }
-            if(Math.ceil(valorIng) >140 && valorIng <=150){
-                resul=3403;
+            if(Math.ceil(valorIng) >140 && valorIng <=160){
+                resul=4151.60;
             }
-            if(Math.ceil(valorIng) >150 && valorIng <=160){
-                resul=3733;
+            if(Math.ceil(valorIng) >160 && valorIng <=180){
+                resul=4554.20;
             }
             if(Math.ceil(valorIng) >180 && valorIng <=200){
-                resul=4028;
+                resul=4914.10;
             }
             if(Math.ceil(valorIng) >200 && valorIng <=250){
-                resul=4836;
+                resul=5899.90;
             }
             if(Math.ceil(valorIng) >250 && valorIng <=300){
-                resul=5402;
+                resul=6590.40;
             }
             if(Math.ceil(valorIng) >300 && valorIng <=350){
-                resul=6144;
+                resul=7495.70;
             }
             if(Math.ceil(valorIng) >350 && valorIng <=400){
-                resul=6932;
+                resul=8457;
             }
             if(Math.ceil(valorIng) >400 && valorIng <=500){
-                resul=8444;
+                resul=10301.70;
             }
             if(Math.ceil(valorIng) >500 && valorIng <=600){
-                resul=9985;
+                resul=12181.70;
             }
             if(Math.ceil(valorIng) >600 && valorIng <=700){
-                resul=11013;
+                resul=13435.80;
             }
             if(Math.ceil(valorIng) >700 && valorIng <=800){
-                resul=12492;
+                resul=15240.10;
             }
             if(Math.ceil(valorIng) >800 && valorIng <=900){
-                resul=13333;
+                resul=16266.20;
             }
-            if(Math.ceil(valorIng) >900 && valorIng <=1000){
-                resul=14803;
+            if(Math.ceil(valorIng) >900 && valorIng < 1000){
+                resul=18059.60;
             }
             
             if(valorIngresoVd == '' || valorIngresoVd < 1000){
@@ -227,7 +227,7 @@ function mostrarFlete(){
                 mensaje.classList.add('resultado');
                 calculadora.appendChild(mensaje);
                 setTimeout(() => {
-                mensaje.remove(), ingreso.value = "", ingresoVd.value = "";
+                mensaje.remove();
              },5000)
             }
             
